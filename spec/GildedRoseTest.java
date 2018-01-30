040ad36a8f120f8a0bf987c47671414938ed0d79
Lars <larsfinlay@googlemail.com>
2018-01-30 13:59:29 +0000

    Tests made

--- spec/GildedRoseTest.java
+++ spec/GildedRoseTest.java
@@ -1,7 +1,4 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 
@@ -43,6 +40,163 @@ public class GildedRoseTest {
         @DisplayName("Item has a quality value that's an integer")
         public void test3() {
             assertEquals(5, app.items.get(0).quality);
+        }
+
+    }
+
+    @Nested
+    @DisplayName("ItemQualityUpdateTests")
+    class ItemQualityUpdateTests {
+
+        @Test
+        @DisplayName("Item quality cannot go below 0")
+        void test1() {
+            app.items.add(new Item("Item", 0, 0));
+            app.updateQuality();
+            assertEquals(0, app.items.get(0).quality);
+        }
+
+        @Test
+        @DisplayName("Item's sellIn value should negate by 1 when updated")
+        void test2() {
+            app.items.add(new Item("Item", 5, 0));
+            app.updateQuality();
+            assertEquals(4, app.items.get(0).sellIn);
+        }
+
+        /*Should an item's sellIn value go below 0?
+        Argument for:
+            -Innkeeper may wan't to keep track of how many days an item is past it's sell by date
+        Argument against:
+            -There is no behaviour difference between an update on an item with sellIn 0 and one with sellIn -1
+         */
+        @Test
+        @Disabled
+        @DisplayName("Item's sellIn value should not go below 0")
+        void test3() {
+            app.items.add(new Item("Item", 0, 0));
+            app.updateQuality();
+            assertEquals(0, app.items.get(0).sellIn);
+        }
+
+        @Nested
+        @DisplayName("StandardItems")
+        class StandardItemTests {
+
+            @Test
+            @DisplayName("A standard item has it's quality negated by 1 when updated")
+            public void test1() {
+                app.items.add(new Item("Standard Item", 5, 10));
+                app.updateQuality();
+                assertEquals(9, app.items.get(0).quality);
+            }
+
+            @Test
+            @DisplayName("A standard item has it's quality negated by 2 when past it's sellby date")
+            public void test2() {
+                app.items.add(new Item("Standard Item", 0, 10));
+                app.updateQuality();
+                assertEquals(8, app.items.get(0).quality);
+            }
+
+        }
+
+        @Nested
+        @DisplayName("MaturingItemsTests")
+        class MaturingItemTests {
+
+            @Test
+            @DisplayName("A maturing item has it's quality increased by 1 when updated")
+            public void test1() {
+                app.items.add(new Item("Aged Brie", 10, 10));
+                app.updateQuality();
+                assertEquals(11, app.items.get(0).quality);
+            }
+
+            @Test
+            @DisplayName("A maturing item has it's quality increased by 2 when updated past it's sellby date")
+            public void test2() {
+                app.items.add(new Item("Aged Brie", 0, 10));
+                app.updateQuality();
+                assertEquals(12, app.items.get(0).quality);
+            }
+
+            @Test
+            @DisplayName("The quality of a maturing item can never go above 50")
+            public void test3() {
+                app.items.add(new Item("Aged Brie", 0, 50));
+                app.updateQuality();
+                assertEquals(50, app.items.get(0).quality);
+            }
+
+        }
+
+        @Nested
+        @DisplayName("ConcertItemTests")
+        class ConcertItemTests {
+
+            @Test
+            @DisplayName("A concert item's value increases in quality by 1 when updated regardless of range")
+            public void test1() {
+                app.items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10));
+                app.updateQuality();
+                assertEquals(11, app.items.get(0).quality);
+            }
+
+            @Test
+            @DisplayName("A concert item's value increases in quality by 2 when updated within 10 days of the concert")
+            public void test2() {
+                app.items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 8, 10));
+                app.updateQuality();
+                assertEquals(12, app.items.get(0).quality);
+            }
+
+            @Test
+            @DisplayName("A concert item's value increases in quality by 3 when updated within 5 days of the concert")
+            public void test3() {
+                app.items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10));
+                app.updateQuality();
+                assertEquals(13, app.items.get(0).quality);
+            }
+
+            @Test
+            @DisplayName("A concert item's value is set to 0 when past it's sellby date")
+            public void test4() {
+                app.items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10));
+                app.updateQuality();
+                assertEquals(0, app.items.get(0).quality);
+            }
+
+            @Test
+            @DisplayName("The quality of a concert ticket can't go above 50")
+            public void test5() {
+                app.items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 4, 50));
+                app.updateQuality();
+                assertEquals(50, app.items.get(0).quality);
+            }
+
+        }
+
+        @Nested
+        @DisplayName("LegendaryItemTests")
+        class LegendaryItemTests {
+
+            @Test
+            @DisplayName("A legendary item never has it's quality altered")
+            public void test1() {
+                app.items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+                app.updateQuality();
+                assertEquals(80, app.items.get(0).quality);
+            }
+
+            @Test
+            @DisplayName("A legendary item never has to be sold")
+            public void test2() {
+                app.items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+                app.updateQuality();
+                assertEquals(0, app.items.get(0).sellIn);
+            }
+
         }
 
     }
